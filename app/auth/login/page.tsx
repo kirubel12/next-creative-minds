9fb317628a12256f5e7622b222f3b9278bf15e3d
kirubel12 <kirubelhabte1991@gmail.com>
2023-10-15 18:04:39 +0100

    Navbar are added

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import React from 'react'
+
+const Login = () => {
+  return (
+    <Card className='max-w-xl w-[400] p-4 text-center justify-center m-auto'>
+      <CardTitle>Join Now</CardTitle>
+      <Button className='w-full text-sm mt-4 dark:bg-primary bg-slate-600'>Sign In With Google</Button>
+    </Card>
+  )
+}
+
+export default Login
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashbaordPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>Dashboard</h1>
+    </div>
+  )
+}
+
+export default DashbaordPage
